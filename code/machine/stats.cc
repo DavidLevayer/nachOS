a8d7c1bc5caa3f48dd07f2480f070377e07d9e9c
Nexucis <nexucis@gmail.com>
2014-01-29 09:11:46 +0100

    ajout du code de nachOS

---
+++ code/machine/stats.cc
@@ -0,0 +1,49 @@
+// stats.h 
+//	Routines for managing statistics about Nachos performance.
+//
+// DO NOT CHANGE -- these stats are maintained by the machine emulation.
+//
+// Copyright (c) 1992-1993 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "utility.h"
+#include "stats.h"
+
+//----------------------------------------------------------------------
+// Statistics::Statistics
+// 	Initialize performance metrics to zero, at system startup.
+//----------------------------------------------------------------------
+
+Statistics::Statistics()
+{
+    totalTicks = idleTicks = systemTicks = userTicks = 0;
+    numDiskReads = numDiskWrites = 0;
+    numConsoleCharsRead = numConsoleCharsWritten = 0;
+    numPageFaults = numPacketsSent = numPacketsRecvd = 0;
+}
+
+//----------------------------------------------------------------------
+// Statistics::Print
+// 	Print performance metrics, when we've finished everything
+//	at system shutdown.
+//----------------------------------------------------------------------
+
+void
+Statistics::Print()
+{
+  // LB: format adapted to long long tick type
+  // printf("Ticks: total %d, idle %d, system %d, user %d\n", totalTicks, 
+  // idleTicks, systemTicks, userTicks);
+  printf("Ticks: total %lld, idle %lld, system %lld, user %lld\n", 
+	 totalTicks, idleTicks, systemTicks, userTicks);
+  // End of correction
+
+    printf("Disk I/O: reads %d, writes %d\n", numDiskReads, numDiskWrites);
+    printf("Console I/O: reads %d, writes %d\n", numConsoleCharsRead, 
+	numConsoleCharsWritten);
+    printf("Paging: faults %d\n", numPageFaults);
+    printf("Network I/O: packets received %d, sent %d\n", numPacketsRecvd, 
+	numPacketsSent);
+}
---
+++ code/threads/bool.h
@@ -0,0 +1,15 @@
+// Defining TRUE and FALSE is usually a Bad Idea,
+// because you will probably be inconsistent with anyone
+// else who had the same clever idea.
+// Therefore:  DON'T USE THIS FILE.
+
+#ifndef _bool_h
+#define _bool_h 1
+
+// 'true' and 'false' are c++ keywords.
+#undef TRUE
+#undef FALSE
+#define TRUE true
+#define FALSE false
+
+#endif
