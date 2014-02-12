76ca9f3f69d25d265608e073a45af56ab2fd4516
david <david@debian>
2014-02-12 11:12:51 +0100

    fichiers synchconsole.c et .h

---
+++ code/userprog/synchconsole.cc
@@ -0,0 +1,44 @@
+//#ifdef CHANGED
+
+#include "copyright.h"
+#include "console.h"
+#include "system.h"
+#include "synch.h"
+#include "synchconsole.h"
+
+static Semaphore *readAvail;
+static Semaphore *writeDone;
+static void ReadAvail(int arg) { readAvail->V(); }
+static void WriteDone(int arg) { writeDone->V(); }
+
+SynchConsole::SynchConsole(char *readFile, char *writeFile)
+{
+	readAvail = new Semaphore("read avail", 0);
+	writeDone = new Semaphore("write done", 0);
+	console = new Console (readFile, writeFile, ReadAvail, WriteDone, 0);
+}
+SynchConsole::~SynchConsole()
+{
+	delete console;
+	delete writeDone;
+	delete readAvail;
+}
+void SynchConsole::SynchPutChar(const char ch)
+{
+	console->PutChar(ch);
+	writeDone->P();
+}
+char SynchConsole::SynchGetChar()
+{
+	readAvail->P();
+	return console->GetChar();
+}
+void SynchConsole::SynchPutString(const char s[])
+{
+
+}
+void SynchConsole::SynchGetString(char *s, int n)
+{
+	
+}
+//#endif // CHANGED
---
+++ code/userprog/synchconsole.h
@@ -0,0 +1,24 @@
+//#ifdef CHANGED
+#ifndef SYNCHCONSOLE_H
+#define SYNCHCONSOLE_H
+#include "copyright.h"
+#include "utility.h"
+#include "console.h"
+class SynchConsole {
+	public:
+	SynchConsole(char *readFile, char *writeFile);
+	// initialize the hardware console device
+	~SynchConsole();
+	// clean up console emulation
+	void SynchPutChar(const char ch);
+	char SynchGetChar();
+	// Unix putchar(3S)
+	// Unix getchar(3S)
+	void SynchPutString(const char *s); // Unix puts(3S)
+	void SynchGetString(char *s, int n);
+	// Unix fgets(3S)
+	private:
+	Console *console;
+};
+#endif // SYNCHCONSOLE_H
+//#endif // CHANGED
