d2bb3829facb9a35eeeb77949ed1f2da92d285ed
Augustin <nexucis@gmail.com>
2014-03-21 15:16:02 +0100

    fin de la fusion...

--- code/test/userpages0.c
+++ code/test/userpages0.c
@@ -2,21 +2,20 @@
 
 void afficheString(char* s) {
 	int nb = 100;
-	int i:
+	int i;
 	for(i=0;i<nb;i++)
 		SynchPutString((char*)s);
+	
 	UserThreadExit();
 }
 
 int main () {	
-	
 	SynchPutString("Demarrage du main...\n");
 
 	// Premier Thread
 	void* f = afficheString;
-    int res = UserThreadCreate(f,&i);
 
-    res = UserThreadCreate(f,"s");
+    int res = UserThreadCreate(f,"s");
     res = UserThreadCreate(f,"z");
     /* not reached */
     return res;
